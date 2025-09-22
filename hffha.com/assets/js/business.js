1b703b806343f75a24aa478de3d0592f8d2ccd51
mono <scsuperman999999@gmail.com>
2025-09-22 16:19:36 +0800

    sc

--- hffha.com/assets/js/business.js
+++ hffha.com/assets/js/business.js
@@ -11,67 +11,25 @@ function colorInit() {
     );
   });
 }
-colorInit();
-let allGamesList = {}
-let categoryGamesList = {}
-async function loadJson(){
-  const baseUrl = "/assets/js/games.json"; // 指向本地文件路径
-  let result = await fetch(baseUrl, {
-    method: "GET",
-  });
-  categoryGamesList = await result.json()
-  if (Array.isArray(categoryGamesList)) {
-    allGamesList = categoryGamesList.flat(); // 正常使用 flat()
-  } else {
-    // 如果 allGamesList 不是数组，可能是对象，需要特殊处理
-    allGamesList = Object.values(categoryGamesList).flat();
-  }
+// colorInit();
 
-}
-await loadJson();
 async function getGamesList() {
   let result = await fetch("./freegames.json", { method: "GET" });
   return await result.json();
 }
 
 async function getGamesListApi(params) {
-  let category = params.category
-  if(category){
-    return categoryGamesList[category]
-  }
-  if (params.name){
-    for (let item of allGamesList) {
-      // console.log("查找:", item.title, " item=",item);
-      if (item.id === params.name) {
-        // console.log("返回了:", params.name, " result=",item);
-        return [item]; // 返回匹配的结果
-      } else if(item.title == params.name){
-        return [item]; // 返回匹配的结果
-      }
-    }
-  }
-  return allGamesList;
+  const baseUrl = "https://gamemonetize.com/feed.php";
+  let assignParams = Object.assign(
+    { format: 0, num: 100, page: 1, platform: 1, links: 1 },
+    params
+  );
+  const queryParams = new URLSearchParams(assignParams);
+  let result = await fetch(`${baseUrl}?${queryParams.toString()}`, {
+    method: "GET",
+  });
+  return await result.json();
 }
+let getGamesListCategory=['IO','2Player','3D','Adventure','Arcade','Bejeweled','Boys','Clicker','Cooking','Girls','Hypercasual','Multiplayer','Puzzle','Racing','Shooting','Sports','Stickman','Baby Hazel']
 
-let getGamesListCategory=[
-  "IO",
-  "2 Player",
-  "3D",
-  "Adventure", 
-  "Arcade",
-  "Bejeweled",
-  "Boys",
-  "Clicker",
-  "Cooking",
-  "Girls",
-  "Hypercasual",
-  "Multiplayer",
-  "Puzzle",
-  "Racing",
-  "Shooting",
-  "Soccer",
-  "Sports",
-  "Stickman",
-  "Baby Hazel"
-]
 export { getGamesList, getGamesListApi,getGamesListCategory };
